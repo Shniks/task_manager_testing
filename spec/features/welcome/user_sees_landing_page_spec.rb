214784cc04d7129d8f8e06f09083c3a3e739dd9f
Nikhil Shahi <shniks@gmail.com>
2022-08-02 13:03:42 -0700

    created test for landing on welcome page.

---
+++ spec/features/welcome/user_sees_landing_page_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "welcome page", type: :feature do
+  describe "When I visit the / page" do
+    it "displays a welcome message and has links to the tasks index page and create a task page" do
+
+      visit '/'
+
+      expect(page).to have_content "Welcome to Task Manager"
+      expect(page).to have_content "Tasks Index"
+      expect(page).to have_content "Create a New Task"
+    end
+  end
+end
