0c7f14a509afe0eeb5b8e44f6c3e5556771a8cf6
Nikhil Shahi <shniks@gmail.com>
2022-08-02 15:59:36 -0700

    Created tasks show page for individual task.

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
   get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show'
 end
