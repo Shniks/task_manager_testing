7b2ad9a3e176fa3ca55e6df13303668109c380a7
Nikhil Shahi <shniks@gmail.com>
2022-08-02 19:03:20 -0700

    Refactored update action in tasks controller.

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -23,7 +23,8 @@ class TasksController < ApplicationController
   end
 
   def update
-    Task.update(task_params)
+    task = Task.find(params[:id])
+    task.update(task_params)
 
     redirect_to '/tasks'
   end
